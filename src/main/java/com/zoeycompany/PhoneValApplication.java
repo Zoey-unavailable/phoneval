afddd8c1f358afe23b62ab6314a30414afef615a
Zoey-unavailable <297405067@qq.com>
2024-03-18 14:20:06 +0800

    手机号验证——redis练手

---
+++ src/main/java/com/zoeycompany/PhoneValApplication.java
@@ -0,0 +1,13 @@
+package com.zoeycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneValApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneValApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoeycompany/phonevaldemo/PhoneValApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoeycompany.phonevaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneValApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
